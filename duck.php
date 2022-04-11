c98f02133a66dbea89b66713faaa4262adb84b2f
Isma Tagiev <tagiev.ismail2017@yandex.ru>
2022-04-11 13:26:46 +0700

    FlyBehavior

--- duck.php
+++ duck.php
@@ -1,5 +1,6 @@
 <?php 
 require_once 'flybehavior.php';
+require_once 'nflybehavior.php';
 
 abstract class Duck {
 	public $flyBehavior;
@@ -11,5 +12,12 @@ abstract class Duck {
 	public function swim() {
 		echo 'Все утки умеют плавать, даже деревянные<br>';
 	}
+	public function quack1(){
+		echo '*Резиновые утки не разговаривают*<br>';
+		echo '*Резиновые умеют плавать, но не умеют крякать*<br>';
+		echo '*Резиновые утки не живые!!*<br>';
+
+	}
+
 }
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 require_once 'mallardduck.php';
+require_once 'rubberduck.php';
 
 $duck1 = new MallardDuck();
 $duck1->display();
 $duck1->performFly();
 $duck1->swim();
+
+$duck2 = new RubberDuck();
+$duck2->quack1();
 
 ?>
